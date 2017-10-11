c285c0373660277444959a3e9ffdd048e0c4697a
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2017-10-11 12:16:38 +0100

    First failing test for component

--- src/app/person/person.component.spec.ts
+++ src/app/person/person.component.spec.ts
@@ -1,10 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PersonComponent } from './person.component';
+import { Person } from '../person';
 
 describe('PersonComponent', () => {
   let component: PersonComponent;
   let fixture: ComponentFixture<PersonComponent>;
+
+  const person: Person = {
+    gender: 'female',
+    name: { title: 'miss', first: 'jocelaine', last: 'da cruz' },
+    picture: {
+      large: 'https://randomuser.me/api/portraits/women/88.jpg',
+      medium: 'https://randomuser.me/api/portraits/med/women/88.jpg',
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/women/88.jpg'
+    }
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +27,12 @@ describe('PersonComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PersonComponent);
     component = fixture.componentInstance;
+    component.person = person;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should display the supplied person', () => {
+    expect(fixture.nativeElement.querySelector('.first-name').textContent)
+        .toBe('Jocelaine');
   });
 });
