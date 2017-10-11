d174f661c33a9bde92983d855190ece9d18d7e6a
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2017-10-11 12:12:57 +0100

    Add types to service and tests

--- src/app/person.service.spec.ts
+++ src/app/person.service.spec.ts
@@ -2,6 +2,7 @@ import { HttpClientTestingModule, HttpTestingController } from '@angular/common/
 import { TestBed } from '@angular/core/testing';
 
 import { PersonService } from './person.service';
+import { Person } from './person';
 
 describe('PersonService', () => {
   let service: PersonService;
@@ -27,7 +28,16 @@ describe('PersonService', () => {
   });
 
   it('should expose the fetched data', done => {
-    const randomUser = {};
+    const randomUser: Person = {
+      gender: 'female',
+      name: { title: 'miss', first: 'marguerite', last: 'lenselink' },
+      picture: {
+        large: 'https://randomuser.me/api/portraits/women/89.jpg',
+        medium: 'https://randomuser.me/api/portraits/med/women/89.jpg',
+        thumbnail: 'https://randomuser.me/api/portraits/thumb/women/89.jpg'
+      }
+    };
+
     service.getRandomUser();
 
     httpMock
--- src/app/person.service.ts
+++ src/app/person.service.ts
@@ -3,17 +3,23 @@ import { Injectable } from '@angular/core';
 
 import { ReplaySubject } from 'rxjs/ReplaySubject';
 
+import { Person } from './person';
+
+export interface ApiResponse {
+  results: Person[];
+}
+
 @Injectable()
 export class PersonService {
-  private randomUserSubject = new ReplaySubject();
+  private randomUserSubject = new ReplaySubject<Person>();
   randomUser$ = this.randomUserSubject.asObservable();
 
   constructor(private http: HttpClient) { }
 
   getRandomUser() {
     this.http
-        .get('https://randomuser.me/api')
-        .subscribe((result: any) => {
+        .get<ApiResponse>('https://randomuser.me/api')
+        .subscribe(result => {
           this.randomUserSubject.next(result.results[0]);
         });
   }
