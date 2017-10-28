837b22f60470f8070bab2ecffda603685488cbfb
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2017-10-28 14:49:21 +0100

    Create component and service

---
+++ src/app/person/person.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pgn-person',
+  templateUrl: './person.component.html',
+  styleUrls: ['./person.component.scss']
+})
+export class PersonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
