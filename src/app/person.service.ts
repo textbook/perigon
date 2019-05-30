75bf0650542b7579bddbf17b59b77db3d923e291
Jonathan Sharpe <jsharpe@pivotal.io>
2019-05-30 11:43:42 +0100

    Simple fetch implementation

--- src/app/person.service.ts
+++ src/app/person.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  getRandomUser() { }
+  getRandomUser() {
+    this.http.get('https://randomuser.me/api').subscribe();
+  }
 }
