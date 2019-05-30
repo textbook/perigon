b027016cdc00f1805b482bb321aaca803ef363de
Jonathan Sharpe <jsharpe@pivotal.io>
2019-05-30 14:23:16 +0100

    Add HttpModule to root app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { PersonComponent } from './person/person.component';
@@ -10,7 +11,8 @@ import { PersonComponent } from './person/person.component';
     PersonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
