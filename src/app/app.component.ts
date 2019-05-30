868a580c3a26c10fb6a6b3cda3881ba18820609b
Jonathan Sharpe <jsharpe@pivotal.io>
2019-05-30 12:11:34 +0100

    Get a random user in the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { PersonService } from './person.service';
 
 @Component({
   selector: 'pgn-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'perigon';
+
+  constructor(private service: PersonService) { }
+
+  ngOnInit() {
+    this.service.getRandomUser();
+  }
 }
