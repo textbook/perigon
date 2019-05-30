2f5a659a4c74c9d6477d989595806fa359e0b764
Jonathan Sharpe <jsharpe@pivotal.io>
2019-05-30 11:56:41 +0100

    First failing test for component

--- src/app/person/person.component.spec.ts
+++ src/app/person/person.component.spec.ts
@@ -1,6 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PersonComponent } from './person.component';
+import { Person } from '../person';
+
+const person: Person = {
+  gender: "male",
+  name: {
+    title: "monsieur",
+    first: "cosimo",
+    last: "vincent"
+  },
+  picture: {
+    large: "https://randomuser.me/api/portraits/men/1.jpg",
+    medium: "https://randomuser.me/api/portraits/med/men/1.jpg",
+    thumbnail: "https://randomuser.me/api/portraits/thumb/men/1.jpg"
+  },
+}
 
 describe('PersonComponent', () => {
   let component: PersonComponent;
@@ -16,10 +31,12 @@ describe('PersonComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PersonComponent);
     component = fixture.componentInstance;
+    component.person = person;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should display the supplied person', () => {
+    expect(fixture.nativeElement.querySelector('.first-name').textContent)
+      .toBe('Cosimo');
   });
 });
--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Person } from '../person';
+
 @Component({
   selector: 'pgn-person',
   templateUrl: './person.component.html',
   styleUrls: ['./person.component.scss']
 })
 export class PersonComponent implements OnInit {
+  person: Person;
 
   constructor() { }
 
