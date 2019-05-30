9dd085b9e5d71bfe93da6405498dce07bfc08da9
Jonathan Sharpe <jsharpe@pivotal.io>
2019-05-30 13:41:36 +0100

    Add failing test for person display

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,34 @@
 import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { Subject } from 'rxjs';
 
 import { AppComponent } from './app.component';
 import { PersonService } from './person.service';
+import { Person } from './person';
+
+const person: Person = {
+  gender: "male",
+  name: {
+    title: "mr",
+    first: "ramon",
+    last: "nuñez"
+  },
+  picture: {
+    large: "https://randomuser.me/api/portraits/men/13.jpg",
+    medium: "https://randomuser.me/api/portraits/med/men/13.jpg",
+    thumbnail: "https://randomuser.me/api/portraits/thumb/men/13.jpg"
+  },
+};
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
   let service: jasmine.SpyObj<PersonService>;
+  let subject: Subject<Person>;
 
   beforeEach(async(() => {
     service = jasmine.createSpyObj('PersonService', ['getRandomUser']);
+    subject = new Subject();
+    service.randomUser$ = subject.asObservable();
+
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
@@ -18,10 +39,20 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+  });
+
   it('should fetch a random user from the service', () => {
-    const fixture: ComponentFixture<AppComponent> = TestBed.createComponent(AppComponent);
+    expect(service.getRandomUser).toHaveBeenCalled();
+  });
+
+  it('should pass the person to the child component', () => {
+    subject.next(person);
     fixture.detectChanges();
 
-    expect(service.getRandomUser).toHaveBeenCalled();
+    expect(fixture.nativeElement.querySelector('.first-name').textContent)
+      .toBe("Ramon");
   });
 });
